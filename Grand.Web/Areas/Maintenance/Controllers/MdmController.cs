74bb5ec59c01bb845433ad814e4582e8c8b4b87a
Sinona Jose <bsm.spa8@hotmail.com>
2020-02-28 17:56:18 +0530

    Updates in MDM

--- Grand.Web/Areas/Maintenance/Controllers/MdmController.cs
+++ Grand.Web/Areas/Maintenance/Controllers/MdmController.cs
@@ -16,68 +16,70 @@ using System.Threading.Tasks;
 namespace Grand.Web.Areas.Maintenance.Controllers
 {
     [Area("Maintenance")]
-    public class MakerController : BaseAdminController
+    public class MdmController : BaseAdminController
     {
         private readonly IMakerService _makerService;
         private readonly IMakerService1 _makerService1;
         private readonly IMakerViewModelService _makerViewModelService;
         private readonly IMakerViewModelService1 _makerViewModelService1;
-        public MakerController(IMakerViewModelService _makerViewModelService, IMakerService _makerService, IMakerViewModelService1 _makerViewModelService1, IMakerService1 _makerService1)
+        public MdmController(IMakerViewModelService _makerViewModelService, IMakerService _makerService, IMakerViewModelService1 _makerViewModelService1, IMakerService1 _makerService1)
         {
-
             this._makerViewModelService = _makerViewModelService;
             this._makerService = _makerService;
             this._makerViewModelService1 = _makerViewModelService1;
             this._makerService1 = _makerService1;
-
         }
 
         // list
         public IActionResult Index() => RedirectToAction("List");
         public async Task<IActionResult> List()
         {
-
             var model = new MakerListModel();
-            var makers = await _makerService.GetAllMakers("Vishnu", 0, 500, true);
+            var makers = await _makerService.GetAllMakers("", 0, 500, true);
             List<Maker> makerList = makers.ToList();
-            var gridModel = new DataSourceResult {
+            var gridModel = new DataSourceResult 
+            {
                 Data = makers.ToList()
             };
 
             return View(makerList);
         }
+
        [HttpPost]
         public async Task<IActionResult> List(DataSourceRequest command, MakerListModel model)
         {
-            var makers = await _makerService.GetAllMakers(model.SearchName, command.Page - 1, command.PageSize, true);
-
-            var gridModel = new DataSourceResult {
-                Data = makers.ToList()
-
-            };
-            return Json(gridModel);
+            var makers = await _makerService.GetAllMakers("", 0, 500, true);
+            return View(makers);      
         }
       
         [HttpGet]
         public async Task<IActionResult> AddMaker()
         {
             var model = await Task.FromResult<object>(null);
-
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> MdmList()
+        {
+            var model = await Task.FromResult<object>(null);
+            var makers = await _makerService.GetAllMakers("", 0, 500, true);
+            return View(makers);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> AddMakerDetails(MakerModel addNewMaker)
         {
-            await _makerViewModelService.PrepareMakerModel(addNewMaker, "Vishnu", true);
-            return RedirectToAction("AddMaker", "Maker");
+            await _makerViewModelService.PrepareMakerModel(addNewMaker, "", true);
+            return RedirectToAction("MdmList", "Mdm");
         }
+
         [HttpGet]
-        public async Task<IActionResult> AddMakerDetails1(MakerModel1 addNewMaker1)
+        public async Task<IActionResult> AddModelDetails(MakerModel1 addNewMaker1)
         {
-            await _makerViewModelService1.PrepareMakerModel(addNewMaker1, "Vishnu", true);
-            return RedirectToAction("List", "Maker");
+            await _makerViewModelService1.PrepareMakerModel(addNewMaker1, "", true);
+            return RedirectToAction("MdmList", "Mdm");
         }
     }
 }
