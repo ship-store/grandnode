f5d7b1b2d12efce175d9c19eaa1481cf7b635c5d
Sinonajose <bsm.spa8@outlook.com>
2020-07-24 13:09:45 +0530

    critical

---
+++ Grand.Web/Areas/Maintenance/Interfaces/ICriticalViewModelService.cs
@@ -0,0 +1,23 @@
+﻿using Grand.Core;
+using Grand.Core.Domain.CriticalEntity;
+using Grand.Core.Domain.DepartmentEntity;
+using Grand.Core.Domain.EquipmentTypeEntity;
+using Grand.Core.Domain.MakerEntity;
+using Grand.Web.Areas.Admin.Interfaces;
+using Grand.Web.Areas.Maintenance.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grand.Web.Areas.Maintenance.Interfaces
+{
+    public interface ICriticalViewModelService
+    {
+        Task<IPagedList<Critical>> GetAllCriticals(string name = "",
+             int pageIndex = 0, int pageSize = int.MaxValue, bool showHidden = false);
+        Task<IPagedList<Critical>> GetAllCriticalAsList(string id);
+        
+        Task PrepareCriticalModel(CriticalModel model1, object p, bool v);
+    }
+}
